95ae86e8135f61f298dbb4fc97f54e7d69fd86a7
arsen15 <ukon1511@gmail.com>
2024-03-07 14:54:29 -0800

    Testing unity with github

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    // Reference to the component RigidBody
+    public Rigidbody2D myRidgidBody2D;
+    public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive) {
+            myRidgidBody2D.velocity = Vector2.up * flapStrength;
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
+    }
+}
